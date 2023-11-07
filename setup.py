b212937e489b651e17f9e78b90e15814bdc49418
Polonez1 <darjus.vasiukevic@gmail.com>
2023-11-07 18:14:56 +0200

    a

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pySQL",
+    version="1.0.0",
+    description="The project is designed to work with MySQL and MSSQL databases. By integrating this project into other python projects, it is possible to select boards, load them and initiate sql queries using the SQL language.",
+    author="Darjus Vasiukevic",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==1.5.3",
+        "SQLAlchemy==2.0.23",
+        "pyodbc==5.0.1",
+        "mysql-connector-odbc",
+        "mysql-connector-python==8.0.30",
+        "urllib3==1.26.12",
+    ],
+)
