f72f74509829356074e7bc84df7c82ff4c90df16
Polonez1 <64654372+Polonez1@users.noreply.github.com>
2023-12-14 17:49:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pySQL",
-    version="1.0.0",
+    version="1.1.0",
     description="The project is designed to work with MySQL and MSSQL databases. By integrating this project into other python projects, it is possible to select boards, load them and initiate sql queries using the SQL language.",
     author="Darjus Vasiukevic",
     packages=find_packages(),
